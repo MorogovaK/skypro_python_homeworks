3fca1bdc4133d2e783e1eb9343e094c8e641d85b
MorogovaK <morogovaksenia@mail.ru>
2024-04-24 22:36:16 +0500

    Задание урока 1

---
+++ lesson1_task_2.py
@@ -0,0 +1,14 @@
+#1 вариант 
+my_age = 26 
+my_age = 29
+print(my_age)
+
+#2 вариант
+my_age_new = 26
+my_age_new = my_age_new + 3
+print(my_age_new)
+
+#3 вариант
+my_age_super_new = 26
+my_age_super_new += 3
+print(my_age_super_new)
