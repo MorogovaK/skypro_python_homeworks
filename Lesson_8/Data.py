156c1938aefffd9a8aa8a0e0fb10eed0f5c684ce
MorogovaK <morogovaksenia@mail.ru>
2024-06-27 14:31:11 +0500

    Lesson8

---
+++ Lesson_8/Data.py
@@ -0,0 +1,9 @@
+import pytest
+import requests
+
+@pytest.fixture()
+def get_token(username='raphael', password='cool-but-crude'):
+    log_pass = {'username': username, 'password': password}
+    resp_token = requests.post(url="https://x-clients-be.onrender.com/" + '/auth/login', json=log_pass)
+    token = resp_token.json()['userToken']
+    return token
